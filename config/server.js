811e80c8ef704a53ce762bcae3cbe8bb04f30f4e
pmiara <piotr.miara97@gmail.com>
2020-11-22 19:28:09 +0100

    Delete a default value for ADMIN_JWT_SECRET
    It is now set in Heroku.

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd05493e91c8604fb5e89f6e1701edb45'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
