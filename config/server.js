bcfe9e71add89ffdc4cf121932ce4fbdd3a7dc97
pmiara <piotr.miara97@gmail.com>
2020-12-03 16:40:41 +0100

    Add a default value for ADMIN_JWT_SECRET env variable
    It makes running the service locally easier. The .env file is no longer needed.

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', 'P6WYKF2OKCx1/DC79cw4tDfCkQ6L5kqMb+w61X/pjUYHWK4ThoaevDE4Es4Kk2uvh65oL55Dq1xcLtEaLcARnQ=='),
     },
   },
 });
